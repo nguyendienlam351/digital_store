4206d3dce61a52cbe233d49ae64f3b4211b09045
Pham Hieu <pvhieuit@gmail.com>
2021-09-01 18:48:12 +0700

    setting add material-ui to nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,46 @@
-import '../styles/globals.css'
-import { DataProvider } from '../store/GlobalState'
-import Notify from '../components/Notify'
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../theme/index";
+import { DataProvider } from "../store/GlobalState";
+import Notify from "../components/Notify";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <DataProvider>
-      <Notify />
-      <Component {...pageProps} />
-    </DataProvider>)
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <DataProvider>
+        <ThemeProvider theme={theme}>
+          <Notify />
+
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </DataProvider>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
