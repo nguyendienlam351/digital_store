9bbf18a9afbfd738eee8f9ac7810a71f211e4c5b
nguyendienlam351 <87943648+nguyendienlam351@users.noreply.github.com>
2021-09-02 15:20:13 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     "MONGODB_URI": "mongodb+srv://admin:Admin123@clusters.5tg5s.mongodb.net/digi_store?retryWrites=true&w=majority",
-    "BASE_URL": "https://digital-store.vercel.app/",
+    "BASE_URL": "https://digital-store.vercel.app",
     "ACCESS_TOKEN_SECRET": "h8~8KN(&",
     "REFRESH_TOKEN_SECRET": "G3xb8)Qb",
     "serviceID": "gmail",
